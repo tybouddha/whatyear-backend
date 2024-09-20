aa3332b6a367f62b38fc00e92af28b888220370d
Tybouddha <bruneecedric@gmail.com>
2024-09-20 16:33:05 +0200

    backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const yearRouter = require('./routes/year')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/year', yearRouter)
 
 module.exports = app;
